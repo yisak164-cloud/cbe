4ea2afbe1ef30722e2dd9df4992b62fbd6c02ae3
yisak164-cloud <yisak164@gmail.com>
2026-07-24 13:08:53 +0300

    Change withCredentials to false in axios config

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"  // ← add this line
 
 export const api = axios.create({
     baseURL: "https://cbe-backend-lovat.vercel.app/api",
-    withCredentials: true,
+    withCredentials: false,
     timeout: 5000
 })
