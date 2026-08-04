be40ec89a6ba60b07c5fbeb144144bf3cd63e2f8
yisak164-cloud <yisak164@gmail.com>
2026-08-04 16:35:43 +0300

    Enhance authMiddleware with logging and error handling
    Added logging for middleware execution and cookie receipt. Improved error handling for token verification.

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,21 +1,29 @@
 import jwt from "jsonwebtoken";
 
 function authMiddleware(req, res, next) {
+
   console.log("Auth middleware reached");
+  console.log("Cookies received:", req.cookies);
+
   const token = req.cookies["extension-cookie"];
+
   if (!token) {
+    console.log("No cookie found");
     return res.status(401).json({ message: "please login first" });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.id = decoded.id;
-    req.role = decoded.role;
-    console.log("Authentication passed");
-next();
-    
 
-  } catch (err) {
-    return res.status(401).send("invalid token");
+    console.log("Decoded token:", decoded);
+
+    req.id = decoded.id;
+
+    next();
+
+  } catch(err) {
+    console.log("JWT error:", err.message);
+    return res.status(401).json({ message:"invalid token" });
   }
 }
 
