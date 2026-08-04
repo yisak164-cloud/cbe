7cbdb29ca4b6bdc6c77e11611e2c19a3e34717f0
yisak164-cloud <yisak164@gmail.com>
2026-08-04 19:25:10 +0300

    Add headers to axios instance configuration

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -4,6 +4,7 @@ export const api = axios.create({
     baseURL: "https://cbe-backend-lovat.vercel.app/api",
     withCredentials: true,
     timeout: 5000,
+    headers:{
+        "Content-Type":"application/json"
+    }
 });
-
-console.log("Axios baseURL:", api.defaults.baseURL);
